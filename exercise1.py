bb34547709f9cd6655ba5aaa782dbe479df5cbef
Ayush Kumar <177103775+ayushkr84@users.noreply.github.com>
2024-11-02 19:37:36 +0530

    Create exercise1.py
    Topic : Introduction to Programming Exercises

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+# Exercise 1
+# Mailing Address
+#  Create a program that displays your name and complete mailing address formatted in
+# the manner that you would usually see it on the outside of an envelope.Your program
+# does not need to read any input from the user.
+print("Ben Stepenson")
+print("Department of computer Science")
+print("University of Calagary")
+print("2500 University Drive NW")
+print("Canada")
